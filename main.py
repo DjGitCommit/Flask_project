f6fd0c1ffc92b566f1a5374caeba7ff632c233d1
Ryan Kojan <rkojan@gmail.com>
2018-05-16 19:59:17 -0700

    commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request,session
+import os
+import uuid
+import hashlib
+app = Flask(__name__)
+
+def hash_password(password):
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+
+def check_password(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+@app.route("/",methods = ['GET'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
